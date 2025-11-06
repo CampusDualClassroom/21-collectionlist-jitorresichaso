486606b9c79f66c711aaf07adcc44ccb2f5cc856
Juan Ignacio Torres Ichaso <jitorresichaso@macrotest.es>
2025-11-06 15:40:40 +0100

    Exercise 21 solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,25 +2,38 @@ package com.campusdual.classroom;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class Exercise {
 
     public static List<String> createArrayList() {
-
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("GKFFD");
+        arrayList.add("TNANA");
+        arrayList.add("MPMSL");
+        arrayList.add("PSWME");
+        arrayList.add("LZMLF");
+        arrayList.add("JYEBV");
+        arrayList.add("YELNT");
+        arrayList.add("JSNKR");
+        arrayList.add("JFESF");
+        arrayList.add("TMJLL");
+        return arrayList;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (int i = 0; i < customList.size(); i++) {
+            System.out.println("Index " + i + ": " + customList.get(i));
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
-
+        List<String> list = createArrayList();
+        printElementsAndIndex(list);
+        addElementToList(list, "AAAAA");
+        printElementsAndIndex(list);
     }
-
-
 }
